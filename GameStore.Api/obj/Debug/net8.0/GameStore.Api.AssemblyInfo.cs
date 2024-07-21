df5a3c961e36006c56b66cd037430bc498aa751e
ahmetaydin0801 <ahmet.aydin@pubinno.com>
2024-07-21 22:59:50 +0300

    create migrations

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653f7d016ed060b777ae362eaa7e14c3c02850d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66af1627d1562469971c3180568acc6542bc39ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
